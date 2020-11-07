8c88b9de4c80b57212faccca9492e8889482684a
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-07 21:03:12 +0000

    Initial commit

---
+++ src/test/kotlin/Tests.kt
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class Tests {
+
+    @Test
+    fun `new board has specified size`() {
+        val board = boardOf(w = 2, h = 3)
+        assertEquals(3, board.height)
+        assertEquals(2, board.width)
+    }
+
+    @Test
+    fun `new board is empty`() {
+        val board = boardOf(w = 2, h = 3)
+        (0 until 2).forEach { col ->
+            (0 until 3).forEach { row ->
+                assertFalse(board.at(row, col))
+            }
+        }
+    }
+
+}
+
+typealias Board = List<List<Boolean>>
+val Board.width: Int get() = this.size
+val Board.height: Int get() = this[0].size
+fun Board.at(row: Int, col: Int): Boolean = this[col][row]
+
+fun boardOf(w: Int, h: Int): Board = (0 until w).map { (0 until h).map { false } }
+
+
